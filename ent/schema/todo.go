ead26d8837ed26914be40d7e3926cce1218a397b
Bodo Kaiser <i@bodokaiser.io>
2021-12-22 10:36:15 +0100

    added ent generated files

--- ent/schema/todo.go
+++ ent/schema/todo.go
@@ -1,6 +1,9 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
 
 // Todo holds the schema definition for the Todo entity.
 type Todo struct {
@@ -9,7 +12,9 @@ type Todo struct {
 
 // Fields of the Todo.
 func (Todo) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("title"),
+	}
 }
 
 // Edges of the Todo.
