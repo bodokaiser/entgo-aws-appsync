455b05356c6af158a6cf593970a5610d069ccb52
Bodo Kaiser <i@bodokaiser.io>
2021-12-25 08:40:16 +0100

    changed database driver in lambda main.go to pgx

--- go.mod
+++ go.mod
@@ -5,6 +5,18 @@ go 1.17
 require (
 	entgo.io/ent v0.9.1
 	github.com/aws/aws-lambda-go v1.27.1
+	github.com/jackc/pgx/v4 v4.14.1
 )
 
-require github.com/google/uuid v1.3.0 // indirect
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.10.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.2.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
--- lambda/main.go
+++ lambda/main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"database/sql"
 	"entgo-aws-appsync/ent"
 	"entgo-aws-appsync/internal/handler"
 	"log"
+	"os"
 
+	entsql "entgo.io/ent/dialect/sql"
+
+	"entgo.io/ent/dialect"
 	"github.com/aws/aws-lambda-go/lambda"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 func main() {
-	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	db, err := sql.Open("pgx", os.Getenv("DATABASE_URL"))
 	if err != nil {
-		log.Fatalf("failed opening connection to sqlite: %v", err)
+		log.Fatalf("failed opening database connection: %v", err)
 	}
+
+	client := ent.NewClient(ent.Driver(entsql.OpenDB(dialect.Postgres, db)))
 	defer client.Close()
 
 	lambda.Start(handler.New(client))
